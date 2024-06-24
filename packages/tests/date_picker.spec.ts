a44eb7ff64bee0dd30173afc22c112aac8770d56
Clare Coates <ccoates@dunelm.com>
2024-06-24 22:05:43 +0100

    added second test to verify results displayed forselected date

--- packages/tests/date_picker.spec.ts
+++ packages/tests/date_picker.spec.ts
@@ -7,13 +7,15 @@ test.describe.only('when user selects dates using the date picker', () => {
   test.beforeEach( async ({ page }) => {
     helpers = new Helpers(page)
     await helpers.goto();
-    const todaysDate = await helpers.getCurrentDate()
-    await helpers.pickTodaysDate(todaysDate)
+    
+   
   })
 
   test('should display fixtures and results for selected date', async ({ page }) => {
-
-    await expect(page).toHaveTitle('Scores & Fixtures - Football - BBC Sport')
+    const todaysDate = await helpers.getCurrentDate()
+    await helpers.pickTodaysDate(todaysDate)
+    
+    await expect(page).toHaveURL(`https://www.bbc.co.uk/sport/football/scores-fixtures/${todaysDate}`)
   });
   
 })
--- packages/tests/utils/helpers.ts
+++ packages/tests/utils/helpers.ts
@@ -3,11 +3,12 @@ import { Locator, Page } from "@playwright/test";
 export class Helpers {
     private readonly page: Page
     private readonly datePicker: Locator
+    private readonly fixtureTable: Locator
 
     constructor(page: Page) {
         this.page = page
         this.datePicker = page.getByTestId('datepicker-dates')
-
+        this.fixtureTable = page.getByTestId('fixtures-page-wrapper')
     }
 
     async goto() {
@@ -16,6 +17,7 @@ export class Helpers {
 
     async getCurrentDate() {
         let currentDate = new Date().toJSON().slice(0,10)
+        return currentDate
     }
 
     async pickTodaysDate(date) {
