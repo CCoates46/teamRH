f4ea28ec8766995adf326ea24f35c263cd0141ca
Clare Coates <ccoates@dunelm.com>
2024-06-24 22:07:32 +0100

    refactor test

--- packages/tests/date_picker.spec.ts
+++ packages/tests/date_picker.spec.ts
@@ -3,17 +3,18 @@ import { Helpers } from './utils/helpers'
 
 test.describe.only('when user selects dates using the date picker', () => {
   let helpers: Helpers
+  let todaysDate
 
   test.beforeEach( async ({ page }) => {
     helpers = new Helpers(page)
     await helpers.goto();
-    
+    todaysDate = await helpers.getCurrentDate()
+    await helpers.pickTodaysDate(todaysDate)
    
   })
 
   test('should display fixtures and results for selected date', async ({ page }) => {
-    const todaysDate = await helpers.getCurrentDate()
-    await helpers.pickTodaysDate(todaysDate)
+    
     
     await expect(page).toHaveURL(`https://www.bbc.co.uk/sport/football/scores-fixtures/${todaysDate}`)
   });
